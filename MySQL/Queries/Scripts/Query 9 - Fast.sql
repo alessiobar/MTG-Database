648ecf83d2415f081f3461a8f97031bc4ec23d44
Alessio Barboni <47196616+alessiobar@users.noreply.github.com>
2023-02-25 15:50:57 +0100

    Add files via upload

---
+++ MySQL/Queries/Scripts/Query 9 - Fast.sql
@@ -0,0 +1,19 @@
+CREATE INDEX pnormal ON prices(normal);
+# DROP INDEX pnormal ON prices;
+
+# EXPLAIN SELECT p.uuid, p.normal
+SELECT p.uuid, p.normal
+FROM prices p
+WHERE p.normal = 0.5 AND p.uuid IN (
+	SELECT ptg.uuid
+	FROM printings ptg
+    WHERE ptg.setCode IN (
+		SELECT s.code
+        FROM sets s
+        WHERE s.releaseDate >= "2000-01-01") AND ptg.name IN (
+			SELECT c.name
+            FROM cards c
+            WHERE c.manaCost RLIKE "[WUBRG]" AND c.name NOT IN(
+				SELECT col.card
+				FROM colors col)))
+ORDER BY p.normal
