c628de46ebadd18131670b5642548f9ad06de1f0
Alessio Barboni <47196616+alessiobar@users.noreply.github.com>
2023-02-25 14:34:19 +0100

    Delete Queries directory

--- Queries/Scripts/Query 2.sql
+++
@@ -1,6 +0,0 @@
-SELECT s.block AS Block, avg(length(c.text)) AS AvgTextLength
-FROM cards c, printings ptg, sets s
-WHERE c.name = ptg.name AND s.code = ptg.setCode
-GROUP BY s.block
-HAVING s.block IS NOT NULL
-ORDER BY AvgTextLength DESC
